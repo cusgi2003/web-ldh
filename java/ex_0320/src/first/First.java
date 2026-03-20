5c7bde363633670f7464b7315e153b69212c5de7
cusgi2003 <dalraexq1@gmail.com>
2026-03-20 17:21:38 +0900

    Create First.java

---
+++ java/ex_0320/src/first/First.java
@@ -0,0 +1,9 @@
+package first;
+
+public class First {
+	// 프로그램이 시작될 때 가장 먼저 실행되는 부분
+	public static void main(String[] args) {
+		// 소괄호 안의 내용을 화면에 출력하는 메서드
+		System.out.println("Hello world");
+	}
+}
