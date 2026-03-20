809edd426c29afca7c3d1eae2e560915a46ad516
cusgi2003 <dalraexq1@gmail.com>
2026-03-20 18:27:52 +0900

    Update Ex1_byteExample.java

--- java/ex_0320/src/ex2_datatype/Ex1_byteExample.java
+++ java/ex_0320/src/ex2_datatype/Ex1_byteExample.java
@@ -1,5 +1,17 @@
 package ex2_datatype;
 
 public class Ex1_byteExample {
-
+	public static void main(String[] args) {
+		// 정수의 경우 해당 자료형이 표현할 수 있는
+		// 범위를 벗어난 데이터를 저장하면
+		
+		// 오버플로우, 언더플로우가 발생할 수 있다.
+		
+		byte var1 = (byte) -129;
+		System.out.println("var1 : " + var1);
+		
+		
+		
+		
+	}
 }
