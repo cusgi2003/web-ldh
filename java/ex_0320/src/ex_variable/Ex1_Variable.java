62241ee6535eeff87221ae57a73f36437b0ef7c1
cusgi2003 <dalraexq1@gmail.com>
2026-03-20 17:54:29 +0900

    Update Ex1_Variable.java

--- java/ex_0320/src/ex_variable/Ex1_Variable.java
+++ java/ex_0320/src/ex_variable/Ex1_Variable.java
@@ -22,7 +22,32 @@ public class Ex1_Variable {
 		x = x+1;
 		System.out.println(x);
 		
+		// 대입은 필요한 만큼 할 수 있다
 		
+		// 선언과 동시에 값을 대입할 수 있다
+		int y = 30;
+		
+		// 변수는 출력문이나 연산식에 사용되어 값을
+		// 활용할 수 있다
+		
+		System.out.println("================");
+		
+		int hour = 3;
+		int minute = 5;
+		System.out.println(hour + "시간 " + minute + "분");
+		
+		// 총 몇분인지 구하기
+		int totalMinute = hour*60 + minute;
+		System.out.println("몇 분 : " + totalMinute + "분");
+		System.out.println("================");
+
+		// 변수는 또 다른 변수에 대입되어 메모리 간에 값을 
+		// 복사할 수 있다
+		
+		int a = 3;
+		int b = a;
+		
+		System.out.println("a : " + a + ", b: " + b);
 		
 		
 	}
