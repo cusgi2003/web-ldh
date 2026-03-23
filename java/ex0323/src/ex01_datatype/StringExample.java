cc25c213deb525ac08cc2ded363017cd27bd5c09
cusgi2003 <dalraexq1@gmail.com>
2026-03-23 11:49:17 +0900

    Create StringExample.java

---
+++ java/ex0323/src/ex01_datatype/StringExample.java
@@ -0,0 +1,12 @@
+package ex01_datatype;
+
+public class StringExample {
+	public static void main(String[] args) {
+		String s1 = "홍길동";
+		System.out.println(s1);
+		System.out.println("홍길동");
+		
+		// 데이터를 변수에 넣어서 사용하는 이유
+		// 데이터를 저장해두고, 재사용하기 위함
+	}
+}
