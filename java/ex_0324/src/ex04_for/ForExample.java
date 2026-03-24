b9cd7be5d52dd4903a3517c2e5b02e020ddf9430
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 17:30:59 +0900

    Update ForExample.java

--- java/ex_0324/src/ex04_for/ForExample.java
+++ java/ex_0324/src/ex04_for/ForExample.java
@@ -2,9 +2,21 @@ package ex04_for;
 
 public class ForExample {
 	public static void main(String[] args) {
+		// 지역 변수 : 특정 영역 내에서 만들어진 변수는 해당 영역 내에서만 사용할 수 있다.
+		// 안에서 만든 변수는 바깥에서 사용할 수 없지만,
+		// 밖에서 만든 변수는 안쪽에서 사용할 수 있다.
 		
-		for(int i = 1; i <= 3; i++) {
+		int i;
+		for(i = 1; i <= 3; i++) {
 			System.out.println(i + " ");
+		}
+		System.out.println("외부 i의 값 : " + i);
+		
+		System.out.println("===========================");
+		
+		// 5부터 1까지 출력
+		for(i =5; i>=1; i--) {
+			System.out.println(i);
 		}
 	}
 }
