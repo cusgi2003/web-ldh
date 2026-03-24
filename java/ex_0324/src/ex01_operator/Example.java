179d00353b61450773ce0601e7304965070c8d34
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 11:40:45 +0900

    Update Example.java

--- java/ex_0324/src/ex01_operator/Example.java
+++ java/ex_0324/src/ex01_operator/Example.java
@@ -1,5 +1,7 @@
 package ex01_operator;
 
+import java.util.Scanner;
+
 public class Example {
 	public static void main(String[] args) {
 		int box = 5;
@@ -8,5 +10,13 @@ public class Example {
 		System.out.println(result1);
 		
 		// ====================
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력받을 값 : \n");
+		int input = sc.nextInt();
+		
+		int result2 = input/box + (input%box == 0 ? 0 : 1);
+		System.out.println("필요한 박스의 개수 : " + result2);
 	}
 }
