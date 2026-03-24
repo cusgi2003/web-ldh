f3b3611dc8d72182fdb584076f1365394bf9615f
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 09:50:49 +0900

    Create StringExample.java

---
+++ java/ex_0323/src/ex01_datatype/StringExample.java
@@ -0,0 +1,35 @@
+package ex01_datatype;
+
+public class StringExample {
+	public static void main(String[] args) {
+		String s1 = "홍길동";
+		
+		System.out.println(s1);
+		System.out.println("홍길동");
+		
+		// 데이터를 변수에 넣어서 사용하는 이유
+		// 데이터를 저장해두고, 재사용하기 위함
+		
+		// 이스케이프 문자
+		// 문자열 안에 특수한 기능을 수행하기 위한 문자
+		System.out.println("안녕하세요 \"홍길동\" 입니다.");
+		
+		// \(백슬래시) 가 붙으면 " 이 문자는 특별한 의미로 처리하라 " 
+		// \n : 줄바꿈
+		System.out.println("hello\nworld");
+		// \t : 탭 1개만큼 들여쓰기
+		System.out.println("hello\tworld");
+		// \" : 큰따옴표 출력
+		// \' : 작은 따옴표 출력
+		// \\ : 백슬래시 출력
+		
+		// java13부터는 텍스트블록 문법을 제공한다.
+		// printn을 쓰거나 \n을 쓸 필요 없이
+		// 작성된 그대로의 문자열을 저장한다.
+		String str = """
+				영역 사이에
+				작성을 합니다.
+				""";
+		System.out.println(str);
+	}
+}
