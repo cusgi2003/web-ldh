35a8947783d0e2d091fa549f0d03d12b6a82097f
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 09:49:50 +0900

    Create DoubleOperatorExample.java

---
+++ java/ex_0323/src/ex05_operator/DoubleOperatorExample.java
@@ -0,0 +1,36 @@
+package ex05_operator;
+
+public class DoubleOperatorExample {
+	public static void main(String[] args) {
+		// 산술연산자
+		// +, -, *, /,  % 연산자가 존재
+		// 곱하기는 x 대신 *를 사용
+		// 나누기는 나누기 기호 대신 /, %를 사용한다
+		
+		byte v1 = 10;
+		byte v2 = 4;
+		byte v3 = 5;
+		long v4 = 10L;
+		
+		int result1 = v1 + v2;	// 모든 피연산자는 int로 변환
+//		int result2 = v1 + v2 - v4;
+		double result3 = v1/v3;
+		
+		int result4 = v1%v2;
+		System.out.println(result4);
+		
+		System.out.println("===================");
+		
+		// 산술변환
+		// 기본적으로 이항 연산자의 두 피연산자의 타입이 일치해야 한다.
+		// 컴퓨너는 서로 다른 타입을 계산하지 못하므로 값의 손실이
+		// 적은쯕으로 타입이 맞춰진다
+		
+		// 관계(비교) 연산자
+		// 크기비교 ( <, <=, >, => )
+		// 동등비교 ( ==, != )
+		
+		
+		
+	}
+}
