3da3d9a28d3b7b1156a82741cc70763c019d2d1e
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 18:19:10 +0900

    Update ForExample.java

--- java/ex_0324/src/ex04_for/ForExample.java
+++ java/ex_0324/src/ex04_for/ForExample.java
@@ -8,23 +8,23 @@ public class ForExample {
 		// 안에서 만든 변수는 바깥에서 사용할 수 없지만,
 		// 밖에서 만든 변수는 안쪽에서 사용할 수 있다.
 		
-		int i;
-		for(i = 1; i <= 3; i++) {
+
+		for(int i = 1; i <= 3; i++) {
 			System.out.println(i + " ");
 		}
-		System.out.println("외부 i의 값 : " + i);
+//		System.out.println("외부 i의 값 : " + i);
 		
 		System.out.println("===========================");
 		
 		// 5부터 1까지 출력
-		for(i =5; i>=1; i--) {
+		for(int i =5; i>=1; i--) {
 			System.out.println(i);
 		}
 		
 		System.out.println("===========================");
 		
 		int sum = 0;
-		for(i = 1; i <= 10; i++) {
+		for(int i = 1; i <= 10; i++) {
 			sum += i;
 		}
 		System.out.println("1~10까지의 총합 : " + sum);
@@ -32,7 +32,7 @@ public class ForExample {
 		System.out.println("===========================");
 		
 		// 1부터 10까지 3의 배수만 출력하는 for문 작성하기
-		for(i = 1; i <= 10; i++) {
+		for(int i = 1; i <= 10; i++) {
 			if(i%3==0) {
 				System.out.print(i + " ");
 			}
@@ -40,7 +40,7 @@ public class ForExample {
 		System.out.println("\n===========================");
 		
 		// 1부터 20까지 홀수만 출력
-		for(i = 1; i <= 20; i++) {
+		for(int i = 1; i <= 20; i++) {
 			if(i%2 != 0) {
 				System.out.print(i + " ");
 			}
@@ -50,7 +50,7 @@ public class ForExample {
 		// 정수형 변수를 하나 초기화 한다
 		// 해당 정수에 해당하는 구구단 출력하기
 		int x = 3;
-		for(i = 1; i < 10; i++) {
+		for(int i = 1; i < 10; i++) {
 			System.out.println(x + " * " +  i + " = " + x*i);
 		}
 		System.out.println("\n===========================");
@@ -62,29 +62,28 @@ public class ForExample {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("정수 입력 : ");
 		int input = sc.nextInt();
-		
-		System.out.println();
+
 		int inputSum = 0;
 		
-		for(i=1; i <= input; i++) {
+		for(int i=1; i <= input; i++) {
 			inputSum += i;
 		}
-		System.out.println("입력받은 수 : "+ input + ", 합계 : "+inputSum);
+		System.out.println("\n입력받은 수 : "+ input + ", 합계 : "+inputSum);
 		
+		System.out.println("===========================");
 		
+		// 10개의 정수를 입력받고, 그 수 중에 짝수가 몇개인지 구하기
+		int res = 0;
+		for(int i=0; i<10; i++) {
+			System.out.println("정수 입력 : ");
+			int resInt = sc.nextInt();
+			if(resInt %2 == 0) {
+				res++;
+			}
+		}
+		System.out.println("짝수의 개수 : " + res);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println("===========================");
+	
 	}
 }
