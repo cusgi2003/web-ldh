fb5d5c5a10f0fd273e9e8caa1d5f19782c40b901
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 14:34:36 +0900

    Update IfElseIfExample.java

--- java/ex_0324/src/ex02_if/IfElseIfExample.java
+++ java/ex_0324/src/ex02_if/IfElseIfExample.java
@@ -10,16 +10,21 @@ public class IfElseIfExample {
 		// 69 	~ 60 	: D
 		// 59 	~ 0 	: F
 		
-		if(score >= 90) {
-			System.out.println("A");
-		} else if(90 > score && score>= 80) {
-			System.out.println("B");
-		} else if(80 > score && score >= 70) {
-			System.out.println("C");
-		}else if(70 > score && score >= 60) {
-			System.out.println("D");
+		// 0보다 작은 수가 들어왔을 때 "다시 입력해주세요" 출력
+		if(score < 0 || score > 100) {
+			System.out.println("100 이하, 0 이상의 숫자만 입력해주세요");
 		} else {
-			System.out.println("F");
+			if(score >= 90) {
+				System.out.println("A");
+			} else if(score>= 80) {
+				System.out.println("B");
+			} else if(score >= 70) {
+				System.out.println("C");
+			}else if(score >= 60) {
+				System.out.println("D");
+			} else {
+				System.out.println("F");
+			}
 		}
 	}
 }
