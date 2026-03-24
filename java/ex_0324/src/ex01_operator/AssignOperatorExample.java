8817502c136a2d2f1185239bf605b5833331a03d
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 10:17:27 +0900

    Create AssignOperatorExample.java

---
+++ java/ex_0324/src/ex01_operator/AssignOperatorExample.java
@@ -0,0 +1,24 @@
+package ex01_operator;
+
+public class AssignOperatorExample {
+	public static void main(String[] args) {
+		// = 
+		// 우변의 값을 좌변에 대입한다.
+		int n1 = 10;
+		int n2 = 7;
+		
+		// 복합대입연산자
+		// 산술대입연산자와 대입연산자가 합쳐진 형태
+		// +=, -=, *=, /=, %=
+		
+		int x = 10;
+		x += 3;	// x = x + 3;
+		System.out.println(x);
+		
+		
+		
+		
+		
+		
+	}
+}
