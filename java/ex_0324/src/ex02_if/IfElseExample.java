0682009b55b93650c15406d134c8d9f5a124c2ea
cusgi2003 <dalraexq1@gmail.com>
2026-03-24 14:15:33 +0900

    Update IfElseExample.java

--- java/ex_0324/src/ex02_if/IfElseExample.java
+++ java/ex_0324/src/ex02_if/IfElseExample.java
@@ -1,5 +1,13 @@
 package ex02_if;
 
 public class IfElseExample {
-
+	public static void main(String[] args) {
+		int age = 17;
+		
+		if(age >= 20) {
+			System.out.println("성입니다");
+		} else {
+			System.out.println("미성년자입니다");
+		}
+	}
 }
