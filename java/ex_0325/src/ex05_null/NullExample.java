ecf0cbf856d693a8141ac66abaa5b85537989868
cusgi2003 <dalraexq1@gmail.com>
2026-03-25 16:05:07 +0900

    Create NullExample.java

---
+++ java/ex_0325/src/ex05_null/NullExample.java
@@ -0,0 +1,11 @@
+package ex05_null;
+
+public class NullExample {
+	public static void main(String[] args) {
+		// NullPointerException
+		// 객체가 ㅇ벗는 상태에서 객체를 사용하려고 해서 발생하는 오류
+		String str = null;
+		
+		System.out.println(str);
+	}
+}
