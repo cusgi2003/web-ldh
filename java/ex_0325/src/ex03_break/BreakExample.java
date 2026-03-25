71570d2cd5f8e9167e5dc8e44e4afd3bc9d8e793
cusgi2003 <dalraexq1@gmail.com>
2026-03-25 15:59:33 +0900

    Update BreakExample.java

--- java/ex_0325/src/ex03_break/BreakExample.java
+++ java/ex_0325/src/ex03_break/BreakExample.java
@@ -54,27 +54,19 @@ public class BreakExample {
 		if(!isMatched) {
 			System.out.println("10번의 기회를 날렸습니다. 정답은" + rand +"입니다.");
 		}
+		
 		System.out.println("===================");
 		
+		// 라벨
+		outer:for(int i = 1; i <= 5; i++) {
+			for(int j = 1; j <=5; j++) {
+				if(j == 3) {
+					System.out.println(i + " " + j);
+					break outer;
+				}
+			}
+		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 }
