518a5b209d08cacf65ef4ba46edb23d740a7df9a
cusgi2003 <dalraexq1@gmail.com>
2026-03-26 18:30:20 +0900

    Update StudentMain.java

--- java/ex_0326/src/ex03_class/StudentMain.java
+++ java/ex_0326/src/ex03_class/StudentMain.java
@@ -1,15 +1,24 @@
 package ex03_class;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StudentMain {
 	public static void main(String[] args) {
-		Student student = new Student("이동현", 23, 85);
+		Student student1 = new Student("이동현", 23, 85);
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("이름을 입력 하세요");
 		String name = sc.next();
+		System.out.println("나이를 입력 하세요");
+		int age = sc.nextInt();
+		System.out.println("성적을 입력 하세요");
+		int score = sc.nextInt();
 		
+		Student inputStudent = new Student(name, age, score);
+		
+		String[][] strStudent = new String[3][];
+		for
 	}
 }
