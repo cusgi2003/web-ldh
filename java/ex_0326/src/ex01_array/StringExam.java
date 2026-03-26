c2da700dbc90c2304be4fe2441b00aa0784db1cf
cusgi2003 <dalraexq1@gmail.com>
2026-03-26 15:35:48 +0900

    Create StringExam.java

---
+++ java/ex_0326/src/ex01_array/StringExam.java
@@ -0,0 +1,14 @@
+package ex01_array;
+
+import java.util.Scanner;
+
+public class StringExam {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String scString = sc.next();
+		
+		for(int i = 0; i < scString.length(); i++) {
+			
+		}
+	}
+}
