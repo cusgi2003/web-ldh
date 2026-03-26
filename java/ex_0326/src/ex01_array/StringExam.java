8bbf64c4756f991cef2a37bccb4f717903acc2f7
cusgi2003 <dalraexq1@gmail.com>
2026-03-26 20:56:34 +0900

    Update StringExam.java

--- java/ex_0326/src/ex01_array/StringExam.java
+++ java/ex_0326/src/ex01_array/StringExam.java
@@ -7,19 +7,23 @@ public class StringExam {
 		// 연속된 문자를 문자+개수 형태로 압축하기.
 		//Ex) aaabbc => a3b2c1
 		
+		System.out.print("입력 받을 문자 : ");
 		Scanner sc = new Scanner(System.in);
 		String scString = sc.next();
 		
-		int count = 1;
+		int count = 1;		// 문자 개수를 확인해주는  변수
+		
+		String result = "";	// 결과 값을 저장할 함수
 		
 		for(int i = 0; i < scString.length(); i++) {
 			if((i < scString.length() -1) && (scString.charAt(i) == scString.charAt(i+1))) {
 				count++;
 				continue;
 			} else {
-				System.out.print(scString.charAt(i) + "" + count);
+				result += scString.charAt(i) + "" + count;
 				count = 1;
 			}
 		}
+		System.out.println("결과 : " + result);
 	}
 }
