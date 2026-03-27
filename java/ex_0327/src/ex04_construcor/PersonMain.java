28c5c285328d7f94aafe0a5fdb7f1236fab71e8d
cusgi2003 <dalraexq1@gmail.com>
2026-03-27 18:08:36 +0900

    Create PersonMain.java

---
+++ java/ex_0327/src/ex04_construcor/PersonMain.java
@@ -0,0 +1,11 @@
+package ex04_construcor;
+
+public class PersonMain {
+	public static void main(String[] args) {
+		Person person1 = new Person("name1", 20);
+		
+		person1.introduce();
+		
+		
+	}
+}
