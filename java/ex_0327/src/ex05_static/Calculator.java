aab9d2a53778883ab1c7c73a1b1557a6e6109ec2
cusgi2003 <dalraexq1@gmail.com>
2026-03-27 18:16:23 +0900

    Update Calculator.java

--- java/ex_0327/src/ex05_static/Calculator.java
+++ java/ex_0327/src/ex05_static/Calculator.java
@@ -1,5 +1,13 @@
 package ex05_static;
 
 public class Calculator {
-
+	// 파이 : 3.141592;
+	static double pi = 3.141592;
+	
+	// 외부에서 두 수를 받아 더하여 반환하는 plus 메서드 작성하기
+	
+	// 외부에서 두 수를 받아 더하여 반환하는 minus 메서드 작성하기
+	
+	static public int plus(int x, int y) {return x + y;}
+	static public int minus(int x, int y) {return x - y;}
 }
