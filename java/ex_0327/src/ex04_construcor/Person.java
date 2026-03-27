f465015c7f087adbad63c85e5f92bd68ef12be92
cusgi2003 <dalraexq1@gmail.com>
2026-03-27 18:08:39 +0900

    Create Person.java

---
+++ java/ex_0327/src/ex04_construcor/Person.java
@@ -0,0 +1,16 @@
+package ex04_construcor;
+
+public class Person {
+	String name;
+	int age;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+
+	public void introduce() {
+		System.out.println("안녕하세요. 저는 " + age + "살 " + name + "입니다.");
+	}
+}
