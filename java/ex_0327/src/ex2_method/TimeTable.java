e721c85695cf54cfe36d27c38585d53840fec6f0
cusgi2003 <dalraexq1@gmail.com>
2026-03-27 14:43:02 +0900

    Create TimeTable.java

---
+++ java/ex_0327/src/ex2_method/TimeTable.java
@@ -0,0 +1,15 @@
+package ex2_method;
+
+import java.util.Scanner;
+
+public class TimeTable {
+	
+	public void showTable(int input) {
+		
+		for(int i = 1; i <= 9; i++) {
+			System.out.printf("%d x %d = %d \n", input, i, input * i);
+		}
+		
+		System.out.println("=================");
+	}
+}
