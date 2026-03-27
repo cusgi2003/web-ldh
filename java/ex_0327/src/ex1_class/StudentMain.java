80bf564843b06c485d9a0a8cdf617f2166bda00d
cusgi2003 <dalraexq1@gmail.com>
2026-03-27 14:42:50 +0900

    Update StudentMain.java

--- java/ex_0327/src/ex1_class/StudentMain.java
+++ java/ex_0327/src/ex1_class/StudentMain.java
@@ -13,8 +13,29 @@ public class StudentMain {
 		gildong.score = 45;
 		
 		Student yonghee = new Student();
-		gildong.name = "영희";
-		gildong.age = 30;
-		gildong.score = 90;
+		yonghee.name = "영희";
+		yonghee.age = 30;
+		yonghee.score = 90;
+		
+		// 배열에는 같은 타입의 데이터만 넣을 수 있다.
+		// 데이터의 타입은 배열 앞에 명시한다.
+		Student[] students = {minsu, gildong, yonghee};
+		System.out.println(students[0].name);
+		
+		System.out.println("=========================");
+		
+		// 배열에 들어있는 내용을 모두 출력하기
+		for(int i = 0; i < students.length; i++) {
+			System.out.println("이름 : " + students[i].name);
+			System.out.println("나이 : " + students[i].age);
+			System.out.println("점수 : " + students[i].score);
+			
+			System.out.println("=========================");
+		}
+		
+		
+		
+		
+		
 	}
 }
