ae77753457312e3d0680478382420663c3e1bb56
cusgi2003 <dalraexq1@gmail.com>
2026-03-27 15:04:58 +0900

    Update TimesTableMain.java

--- java/ex_0327/src/ex2_method/TimesTableMain.java
+++ java/ex_0327/src/ex2_method/TimesTableMain.java
@@ -11,6 +11,10 @@ public class TimesTableMain {
 		System.out.print("입력받을 단 : \n");
 		int input = sc.nextInt();
 		
+		
+		// return을 통해 반환값을 받는 경우, 연산결과를 변수에 대입한다
+		// 반환값이 없는 경우 호출만 한다 
+		
 		timeTable.showTable(input);
 	}
 }
