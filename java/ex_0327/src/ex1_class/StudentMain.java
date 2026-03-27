92fbd7524a7d832f4e8da4ff264a3d54e17632ca
cusgi2003 <dalraexq1@gmail.com>
2026-03-27 10:12:14 +0900

    Create StudentMain.java

---
+++ java/ex_0327/src/ex1_class/StudentMain.java
@@ -0,0 +1,20 @@
+package ex1_class;
+
+public class StudentMain {
+	public static void main(String[] args) {
+		Student minsu = new Student();
+		minsu.name = "민수";
+		minsu.age = 20;
+		minsu.score = 80;
+		
+		Student gildong = new Student();
+		gildong.name = "길동";
+		gildong.age = 21;
+		gildong.score = 45;
+		
+		Student yonghee = new Student();
+		gildong.name = "영희";
+		gildong.age = 30;
+		gildong.score = 90;
+	}
+}
