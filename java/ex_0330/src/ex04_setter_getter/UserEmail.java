5ce2731cfabb89c8230e7463b412579ad1a79ddb
cusgi2003 <dalraexq1@gmail.com>
2026-03-30 16:02:53 +0900

    Create UserEmail.java

---
+++ java/ex_0330/src/ex04_setter_getter/UserEmail.java
@@ -0,0 +1,16 @@
+package ex04_setter_getter;
+
+public class UserEmail {
+	public static void main(String[] args) {
+		User user = new User();
+		
+		user.setUserName("Hong");
+		user.setPassword("qddqedde");
+		user.setEmail("email@email.com");
+		
+		user.getEmail();
+		user.getPassword();
+		user.getEmail();
+		
+	}
+}
