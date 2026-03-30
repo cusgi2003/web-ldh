cfa6fe6997ab14b767b0498990596a8486ae497f
cusgi2003 <dalraexq1@gmail.com>
2026-03-30 18:30:33 +0900

    Create Pen.java

---
+++ java/ex_0330/src/ex06_inheritance/Pen.java
@@ -0,0 +1,17 @@
+package ex06_inheritance;
+
+public class Pen {
+	public int amount;
+	
+	public Pen(int amount) {
+		this.amount = amount;
+	}
+	
+	public int getAmount(int amount) {
+		return amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
