3368d5192528fd4e3e057c612df4146260a6047e
cusgi2003 <dalraexq1@gmail.com>
2026-03-30 12:15:27 +0900

    Update Parking.java

--- java/ex_0330/src/ex01_final/Parking.java
+++ java/ex_0330/src/ex01_final/Parking.java
@@ -5,11 +5,11 @@ public class Parking {
 	// 기본요금 : 10000원(상수)
 	// 추가 요금 : 시간당 2000원(상수)
 	
-	static int primary_charge = 10000;
-	static int plus_charge = 2000;
+	static int PRIMARY_CHARGE = 10000;
+	static int PLUS_CHARGE = 2000;
 	
 	// 요금 계산 메서드
 	public int resultCharge(int hour) {
-		return primary_charge + (plus_charge * hour);
+		return PRIMARY_CHARGE + (PLUS_CHARGE * hour);
 	}
 }
