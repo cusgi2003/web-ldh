f3081bde939aaf26dbf50f19f0a7fa9a1e707b32
cusgi2003 <dalraexq1@gmail.com>
2026-03-30 17:09:55 +0900

    Create Singleton.java

---
+++ java/ex_0330/src/ex05_singleton/Singleton.java
@@ -0,0 +1,16 @@
+package ex05_singleton;
+
+public class Singleton {
+	// 클래스 내부에서 객체를 생성함
+	private static Singleton sigleton = new Singleton();
+	
+	// private 접근 제한을 갖는 생성자를 선언
+	private Singleton() {
+		
+	}
+	
+	 public static Singleton getInstance() {
+		 return sigleton;
+	 }
+	
+}
