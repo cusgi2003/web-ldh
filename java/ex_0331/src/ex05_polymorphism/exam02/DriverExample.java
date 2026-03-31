8e72b4344f9c8b58296bd4995eac2161a212077e
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 17:12:19 +0900

    Create DriverExample.java

---
+++ java/ex_0331/src/ex05_polymorphism/exam02/DriverExample.java
@@ -0,0 +1,10 @@
+package ex05_polymorphism.exam02;
+
+public class DriverExample {
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		driver.drive(new Vehicle());
+		driver.drive(new Bus());
+		driver.drive(new Texi());
+	}
+}
