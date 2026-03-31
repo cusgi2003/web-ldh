2cfe240ac75c44c2269984fc94e14d4602fe45db
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 17:45:12 +0900

    Update AnimalMain.java

--- java/ex_0331/src/ex05_polymorphism/exam02/AnimalMain.java
+++ java/ex_0331/src/ex05_polymorphism/exam02/AnimalMain.java
@@ -1,9 +1,19 @@
 package ex05_polymorphism.exam02;
 
+// 추상 클래스
 abstract class Animal {
+	
+	// 추상클래스에도 ㅇ리반적인 필드나 메서드가 들어갈 수 있다
+	int feed;
+	public void breath() {
+		System.out.println("숨쉰다");
+	}
+	
 	public abstract void sound() ;
 }
 
+// 추상클래스를 상속받은 클래스는 추상 메서드를 반드시 
+// 오버라이딩해서 내용을 구현해야 한다
 class Dog extends Animal{
 	@Override
 	public void sound() {
@@ -27,7 +37,10 @@ class Zoo {
 public class AnimalMain {
 	public static void main(String[] args) {
 		Zoo zoo = new Zoo();
-		zoo.printSound(new Animal());
+		
+		// 추상클랫는 객체를 직접 생성할 수 없다
+		// Zoo z = new Animal();
+		
 		zoo.printSound(new Dog());
 		zoo.printSound(new Cat());
 	}
