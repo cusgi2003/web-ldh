0aa0a78ae534f1b672a38dae3e26a7899092aa7e
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 10:37:41 +0900

    Update SharpPencil.java

--- java/ex_0330/src/ex06_inheritance/SharpPencil.java
+++ java/ex_0330/src/ex06_inheritance/SharpPencil.java
@@ -3,7 +3,11 @@ package ex06_inheritance;
 public class SharpPencil extends Pen{
 	private int width; //펜의 굵기
 	
-	public SharpPencil(int amount, int width) {
-		super(amount, width);
+	public int getWidth(int width) {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
 	}
 }
