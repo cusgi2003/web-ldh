3d0c0ecbdfcf34ca2f7e9a2107aa06993f1be680
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 10:36:18 +0900

    Update Pen.java

--- java/ex_0330/src/ex06_inheritance/Pen.java
+++ java/ex_0330/src/ex06_inheritance/Pen.java
@@ -1,11 +1,8 @@
 package ex06_inheritance;
 
+// 공통적인 내용을 담을 Pen 클래스
 public class Pen {
-	public int amount;
-	
-	public Pen(int amount) {
-		this.amount = amount;
-	}
+	private int amount;
 	
 	public int getAmount(int amount) {
 		return amount;
