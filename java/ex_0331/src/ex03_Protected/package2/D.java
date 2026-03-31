38e34eb936b7d188dd235d93593599c8db04276e
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 14:45:10 +0900

    Create D.java

---
+++ java/ex_0331/src/ex03_Protected/package2/D.java
@@ -0,0 +1,19 @@
+package ex03_Protected.package2;
+
+import ex03_Protected.package1.A;
+
+public class D extends A{
+	public D() {
+		super(); // A 클래스의 생성자 호출
+	}
+	
+	public void method1() {
+		// 상속받은 필드나 메서드를 사용할 수 있다
+		this.field = "value";
+		this.method();
+	}
+	
+	public void method2() {
+//		A a = new A();
+	}
+}
