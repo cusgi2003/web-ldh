504881284399b1050b0dbd1753ebebba2c9ae85c
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 17:38:05 +0900

    Update AnimalMain.java

--- java/ex_0331/src/ex05_polymorphism/exam02/AnimalMain.java
+++ java/ex_0331/src/ex05_polymorphism/exam02/AnimalMain.java
@@ -1,5 +1,29 @@
 package ex05_polymorphism.exam02;
 
+abstract class Animal {
+	public abstract void sound() ;
+}
+
+class Dog extends Animal{
+	@Override
+	public void sound() {
+		System.out.println("멍멍");
+	}
+}
+
+class Cat extends Animal{
+	@Override
+	public void sound() {
+		System.out.println("야옹");
+	}
+}
+
+class Zoo {
+	public void printSound(Animal animal) {
+		animal.sound();
+	}
+}
+
 public class AnimalMain {
 	public static void main(String[] args) {
 		Zoo zoo = new Zoo();
