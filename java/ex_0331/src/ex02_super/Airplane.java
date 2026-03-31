7ede5407703e6f97cdc2e1e43ce7ccb5542ec9d0
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 13:47:14 +0900

    Create Airplane.java

---
+++ java/ex_0331/src/ex02_super/Airplane.java
@@ -0,0 +1,19 @@
+package ex02_super;
+
+public class Airplane {
+	// "착륙합니다" 라고 출력하는 land() 메서드
+	// "일반 비행합니다" 라고 출력하는 fly()
+	// "이륙합니다 takeOff()
+	
+	public void land() {
+		System.out.println("착륙합니다");
+	}
+	
+	public void fly() {
+		System.out.println("일반 비행합니다");
+	}
+	
+	public void takeOff() {
+		System.out.println("이륙합니다");
+	}
+}
