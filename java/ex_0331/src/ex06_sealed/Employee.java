49853430af63343cab4a00db80411cd965af313b
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 17:54:11 +0900

    Create Employee.java

---
+++ java/ex_0331/src/ex06_sealed/Employee.java
@@ -0,0 +1,11 @@
+package ex06_sealed;
+
+// 봉일된 클래스를 상속하는 자식 클래스는 final 또는
+// non-sealed 키워드로 선언하거나 sealed 키워드를 사용해서
+// 또 다른 봉인 클래스로 선언해야 한다
+public non-sealed class Employee  extends Person{
+	@Override
+	public void work() {
+		System.out.println("제품을 생산합니다");
+	}
+}
