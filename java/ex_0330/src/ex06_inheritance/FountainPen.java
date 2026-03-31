ba1e477acd75f91984e995b90820af9f2cbb8b89
cusgi2003 <dalraexq1@gmail.com>
2026-03-31 10:36:09 +0900

    Update FountainPen.java

--- java/ex_0330/src/ex06_inheritance/FountainPen.java
+++ java/ex_0330/src/ex06_inheritance/FountainPen.java
@@ -1,9 +1,7 @@
 package ex06_inheritance;
 
 public class FountainPen extends BallPen{
-    private String color; //볼펜의 색
-    public String getColor(){return color;}
-    public void setColor(String color){this.color = color;}
+
     public void refill(int n){setAmount(n);}
 
 }
