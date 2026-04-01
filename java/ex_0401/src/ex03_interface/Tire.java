12d8693d275cd14a06dd68b5260b9f915d5591ef
cusgi2003 <dalraexq1@gmail.com>
2026-04-01 17:14:06 +0900

    Update Tire.java

--- java/ex_0401/src/ex03_interface/Tire.java
+++ java/ex_0401/src/ex03_interface/Tire.java
@@ -1,5 +1,11 @@
 package ex03_interface;
 
 public interface Tire {
-
+	void roll();
+	
+	// hankookTire -> 한국타이어가 굴러갑니다
+	// KumhoTire -> 금호타이어가 굴러갑니다
+	
+	// Car 클래스 만들기
+	// Tire타입의 필드 두개 만들고 객체 넣기
 }
