e77f1aa00884c4068c7ac1a1b66eea0cdf79c465
cusgi2003 <dalraexq1@gmail.com>
2026-04-01 13:39:14 +0900

    Update TransportMain.java

--- java/ex_0401/src/ex01_abstact/TransportMain.java
+++ java/ex_0401/src/ex01_abstact/TransportMain.java
@@ -2,10 +2,12 @@ package ex01_abstact;
 
 public class TransportMain {
 	public static void main(String[] args) {
-        Transport[] transports = new Transport[2];
-        
-        transports[0] = new Bus("버스", 1200);
-        transports[1] = new Taxi("택시", 3000, 10, 100);
+        Transport[] transports = {
+        		new Bus("버스", 1200),
+        		new Taxi("택시", 3000, 10, 100),
+        		new AirPlane("비행기", 100000, 50000, 200000)
+        };
+
 
         System.out.println("=== 교통 수단 요금 정보 ===");
         for (Transport t : transports) {
