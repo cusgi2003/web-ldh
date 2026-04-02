1649221d55ba50264975b7050651729e77548246
cusgi2003 <dalraexq1@gmail.com>
2026-04-02 13:56:15 +0900

    Create RemoteControl.java

---
+++ java/ex_0402/src/ex01_innerclass/interfaceexample/RemoteControl.java
@@ -0,0 +1,13 @@
+package ex01_innerclass.interfaceexample;
+
+public class RemoteControl {
+	String power = "Off";
+	
+	class Button{
+		void press() {
+			power = "On";
+			System.out.println("전원을 켭니다");
+		}
+	}
+
+}
