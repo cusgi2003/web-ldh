0131a0d7d10a56c8fde0c95d5d56edc843d2bdd5
cusgi2003 <dalraexq1@gmail.com>
2026-04-02 13:55:51 +0900

    Update Button.java

--- java/ex_0402/src/ex01_innerclass/interfaceexample/Button.java
+++ java/ex_0402/src/ex01_innerclass/interfaceexample/Button.java
@@ -1,5 +1,29 @@
 package ex01_innerclass.interfaceexample;
 
 public class Button {
-
+	
+	//static 붙이지 않아도 자동으로 static 처리가 된다
+	// 중촙 interface는 무조건 static이다(암묵적 static)
+	public static interface ClickListener{
+		void onClick(); // 추상메서드
+	}
+	
+	// 필드
+	private ClickListener clickListener;
+	
+	// setter
+	public void setClickListener(ClickListener clickListener) {
+		this.clickListener = clickListener;
+	}
+	
+	// 버튼이 클릭되엇을 때 실행할 메서드로 click() 메서드 추가하기
+	// 실행 내요은 CLickListener인터페이스 필드를 이용해서 onClick 추상 메서드 호출하기
+	
+	public void click() {
+		clickListener.onClick();
+	}
+	
+	
+	
+	
 }
