514484b767a4de632d308ca57e35e782052945c0
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 10:55:14 +0900

    Create MemberService.java

---
+++ java/service/src/service/MemberService.java
@@ -0,0 +1,23 @@
+package service;
+
+import member.Member;
+
+// 실제 기능을 만드는 곳
+public class MemberService {
+	
+	// 매개변수 자리에 들어올것은 member객체
+	public void register(Member member) {
+		System.out.println("회원 등록을 시작합니다");
+		System.out.println("회원 이름 : " + member.getName());
+		System.out.println("회원 나이 : " + member.getAge());
+		System.out.println("회원 등록이 완료되었습니다.");
+	}
+	
+	public void checkAdult(Member member) {
+		if(member.getAge() >= 20) {
+			System.out.println("성인 회원입니다");
+		} else {
+			System.out.println("미성년 회원입니다");
+		}
+	}
+}
