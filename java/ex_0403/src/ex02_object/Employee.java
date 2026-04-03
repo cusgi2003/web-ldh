18caa0334d1ab08bb7884ee642206ff702896d60
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 17:50:14 +0900

    Create Employee.java

---
+++ java/ex_0403/src/ex02_object/Employee.java
@@ -0,0 +1,12 @@
+package ex02_object;
+
+public class Employee {
+	String name;
+	String dept;
+	
+	public Employee(String name, String dept) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.dept = dept;
+	}
+}
