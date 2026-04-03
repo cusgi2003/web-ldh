f98984fe89aedf225c57b8dfe2fdb58c1c217e4a
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 18:01:48 +0900

    Update Main.java

--- java/ex_0403/src/ex02_object/Main.java
+++ java/ex_0403/src/ex02_object/Main.java
@@ -10,7 +10,25 @@ public class Main {
 		System.out.println(p2);
 		System.out.println(p3);
 		
-		Object[] arr = {"java", 100, 3.14, new Employee}
+		Object[] arr = {"java", 100, 3.14, new Employee("홍길동", "개발팀")};
+		
+		// 반복문으로 배열을 순회하면서
+		// 문자열이면 "문자열데이터 : xx"
+		// 정수면 "정수형 데이터 : xx"
+		// 실수면 "실수형 데이터 : xx"
+		// Employee 객체면 이름과 부서를 출력
+		
+		for(Object obj : arr) {
+			if(obj instanceof String) {
+				System.out.println("문자열 데이터 : " + obj);
+			} else if(obj instanceof Integer) {
+				System.out.println("정수형 데이터 : " + obj);
+			} else if(obj instanceof Double) {
+				System.out.println("실수형 데이터 : " + obj);
+			} else if(obj instanceof Employee emp) {
+				System.out.println("이름 : " + emp.name + ", 부서 : " + emp.dept);
+			}	
+		}
 		
 	}
 }
