0193746619f64be2852838620a342c6fcb18a345
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 17:50:16 +0900

    Create Main.java

---
+++ java/ex_0403/src/ex02_object/Main.java
@@ -0,0 +1,16 @@
+package ex02_object;
+
+public class Main {
+	public static void main(String[] args) {
+		Product p1 = new Product("키보드", 5000, 10);
+		Product p2 = new Product("마우스", 15000, 20);
+		Product p3 = new Product("모니터", 105000, 100);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+		Object[] arr = {"java", 100, 3.14, new Employee}
+		
+	}
+}
