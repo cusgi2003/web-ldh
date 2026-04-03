7d6aa5ac91f0f233b712c18a8e97134b6f0537e5
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 16:39:35 +0900

    Update Main.java

--- java/ex_0403/src/ex01_exception/service/Main.java
+++ java/ex_0403/src/ex01_exception/service/Main.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		OrderService or = new OrderService();
+		Convertor2 convertor = new Convertor2();
+		OrderService os = new OrderService();
+		
+		try {
+			System.out.println("상품 가격 입력 :");
+			String priceStr = sc.next();
+			
+			System.out.println("상품 수량 : ");
+			String quantityStr = sc.next();
+			
+			int price = convertor.toInt(priceStr);
+			int quantity = convertor.toInt(quantityStr);
+			
+			int total = os.calculator(price, quantity);
+			System.out.println("총 금액 : " + total);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
