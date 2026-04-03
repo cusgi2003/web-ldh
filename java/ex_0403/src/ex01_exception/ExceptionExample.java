c6716f4cc593544a0916956a7dd0073d71de702c
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 12:07:33 +0900

    Update ExceptionExample.java

--- java/ex_0403/src/ex01_exception/ExceptionExample.java
+++ java/ex_0403/src/ex01_exception/ExceptionExample.java
@@ -1,5 +1,7 @@
 package ex01_exception;
 
+import java.util.Scanner;
+
 public class ExceptionExample {
 	public static void main(String[] args) {
 		// NullPointerException
@@ -11,7 +13,24 @@ public class ExceptionExample {
 		// NumberFormatException
 		// 잘못된 문자열을 숫자로 형변환할 때 발생
 		
-		String str01 = "11.2";
-		int num01 = Integer.parseInt(str01);
+//		String str01 = "11.2";
+//		int num01 = Integer.parseInt(str01);
+		
+		// ArrayIndenxOutOfBoundsException
+		// 배열에서 인덱스 범위를 초과해 사용할 때 발생한다
+		int [] arr = {1,2,3,4,5};
+		System.out.println(arr[6]);
+		
+		
+		// ArithmeticExcepton
+		// 정수를 0으로 나누려고 할 때 발생
+		System.out.println(10/0);
+		
+		// InputMismatchException
+		// 입력 메서드와 입력한 값의 타입이 맞지 않을 때
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력 : ");
+		int num = sc.nextInt();
+		
 	}
 }
