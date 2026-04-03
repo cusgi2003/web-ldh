f7a8a3b9fca220242c1b8bced7bf66c205005371
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 16:17:01 +0900

    Create Main.java

---
+++ java/ex_0403/src/ex01_exception/service/Main.java
@@ -0,0 +1,10 @@
+package ex01_exception.service;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		OrderService or = new OrderService();
+	}
+}
