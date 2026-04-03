e002d41bdb71bd34a467dcd038c1e184ce2e532a
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 17:13:58 +0900

    Create ToStringExample.java

---
+++ java/ex_0403/src/ex02_object/ToStringExample.java
@@ -0,0 +1,12 @@
+package ex02_object;
+
+public class ToStringExample {
+	public static void main(String[] args) {
+		Object obj = new Object();
+		
+		obj.toString();
+		
+		SmartPhone myPhone = new SmartPhone("삼성전자", "안드로이드");
+		System.out.println(myPhone);
+	}
+}
