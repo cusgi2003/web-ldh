644649325602ec97039e4a004f8aeb346c897de7
cusgi2003 <dalraexq1@gmail.com>
2026-04-03 18:15:41 +0900

    Update EqualsExample.java

--- java/ex_0403/src/ex02_object/EqualsExample.java
+++ java/ex_0403/src/ex02_object/EqualsExample.java
@@ -2,9 +2,9 @@ package ex02_object;
 
 public class EqualsExample {
 	public static void main(String[] args) {
-		Member obj1 = new Member("blue");
-		Member obj2 = new Member("blue");
-		Member obj3 = new Member("red");
+		Member obj1 = new Member("name1", "info1");
+		Member obj2 = new Member("name2", "info2");
+		Member obj3 = new Member("name3", "info3");
 		
 		if(obj1.equals(obj2)) {
 			System.out.println("obj1과 obj2는 동일합니다");
