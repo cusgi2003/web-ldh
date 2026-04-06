4161f9374815329c068d2b03f2b5ccd434262b5a
cusgi2003 <dalraexq1@gmail.com>
2026-04-06 17:05:48 +0900

    Update LocalDateTimeExample.java

--- java/ex_0406/src/ex06_date/LocalDateTimeExample.java
+++ java/ex_0406/src/ex06_date/LocalDateTimeExample.java
@@ -1,6 +1,8 @@
 package ex06_date;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeExample {
 	public static void main(String[] args) {
@@ -34,5 +36,55 @@ public class LocalDateTimeExample {
 		// 년 더하기 : plusYears(숫자)
 		// 빼기 : minusDay(), minusMonths(), minusYears() 
 		
+		// 문자열로 출력형식 바꾸기
+		DateTimeFormatter formatter = 
+				DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초");
+		// yyyy : 년
+		// MM :월
+		// dd : 일
+		// HH : 시
+		// mm : 분
+		// ss : 초
+		String result = now.format(formatter);
+		
+		System.out.println(result);
+		
+		// 문자열을 localdateTime으로 바꾸기
+		String str = "2026/04/06 16:37";
+		formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		LocalDateTime dateTime2 = LocalDateTime.parse(str,formatter);
+		
+		System.out.println(dateTime2);
+		
+		// 날짜 비교 가능
+		// isbefore() : 앞선 시간인지 비교
+		// isAfter() : 뒤의 시간인지 비교
+		LocalDateTime meating = LocalDateTime.of(2026, 4, 10, 14, 0);
+		
+		System.out.println(now.isBefore(meating));
+		System.out.println(now.isAfter(meating));
+		System.out.println(now.isEqual(meating));
+		
+		// 날짜의 특정 부분만 바꾸기
+		// 이미 만들어진 날짜/시간 객체에서
+		// 연도나 월, 일, 시를 바꾼 새로운 객체를 만들 수 있다
+		LocalDateTime changed = now.withYear(2030)
+				.withMonth(12)
+				.withDayOfMonth(25)
+				.withHour(9);
+		System.out.println(now);
+		System.out.println(changed);
+		
+		// 현재 시간이 09:00 ~ 18:00  사이인지 확인하여
+		// 예약 가능 여부를 출력
+		
+		LocalDateTime start  = now.withHour(9).withMonth(0).withSecond(0);
+		LocalDateTime end  = now.withHour(18).of(2026, 4, 6 ,18, 0);
+		
+		if(now.isAfter(start) && now.isBefore(end)) {
+			System.out.println("예약 가능");
+		} else {
+			System.out.println("예약 불가");
+		}
 	}
 }
