5b3d0bedb91e54edf5fab4f052790e7c4908b109
cusgi2003 <dalraexq1@gmail.com>
2026-04-06 11:45:17 +0900

    Update Main.java

--- java/ex_0406/src/ex01_lombok/Main.java
+++ java/ex_0406/src/ex01_lombok/Main.java
@@ -1,5 +1,14 @@
 package ex01_lombok;
 
 public class Main {
-
+	public static void main(String[] args) {
+//		Member m = new Member();
+//		m.setId("2026002");
+//		m.setName("박길동");
+//		m.setAge(30);
+		
+		Member m2 = new Member("2026001", "홍길동", 40);
+		System.out.println(m2);
+		
+	}
 }
