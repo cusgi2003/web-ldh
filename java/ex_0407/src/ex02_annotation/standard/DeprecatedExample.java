c6e0ee346d1c50aef9a79e0e917e3b22b7a6e3c9
cusgi2003 <dalraexq1@gmail.com>
2026-04-07 13:42:34 +0900

    Create DeprecatedExample.java

---
+++ java/ex_0407/src/ex02_annotation/standard/DeprecatedExample.java
@@ -0,0 +1,14 @@
+package ex02_annotation.standard;
+
+public class DeprecatedExample {
+	
+	
+	@Deprecated	// 더이상 사용을 권장하지 않는 기능에 붙임
+	public static void oldMethod() {
+		System.out.println("오래된 메서드");
+	}
+	
+	public static void main(String[] args) {
+		oldMethod();
+	}
+}
