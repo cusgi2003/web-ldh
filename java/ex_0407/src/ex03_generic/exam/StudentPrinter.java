de7c8ec8d35bb45ab90a448c357e983756de4034
cusgi2003 <dalraexq1@gmail.com>
2026-04-07 18:10:07 +0900

    Update StudentPrinter.java

--- java/ex_0407/src/ex03_generic/exam/StudentPrinter.java
+++ java/ex_0407/src/ex03_generic/exam/StudentPrinter.java
@@ -1,11 +1,13 @@
 package ex03_generic.exam;
 
-public class StudentPrinter<T,U> {
-	private T name;
-	private U age;
+public class StudentPrinter{
 	
-	public void set(T name, U age) {
-		this.name = name;
-		this.age = age;
+	public <T, U> void printStudent(T name, U age) {
+		System.out.println("이름 : " + name + ", 나이 : " + age);
+	}
+	
+	public static void main(String[] args) {
+		StudentPrinter sp = new StudentPrinter();
+		sp.printStudent("김철수", 20);
 	}
 }
