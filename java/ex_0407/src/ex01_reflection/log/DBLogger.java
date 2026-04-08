6884fd1a524dfb0ea471a1881239b1bf9983a7e9
cusgi2003 <dalraexq1@gmail.com>
2026-04-08 10:55:31 +0900

    Create DBLogger.java

---
+++ java/ex_0407/src/ex01_reflection/log/DBLogger.java
@@ -0,0 +1,9 @@
+package ex01_reflection.log;
+
+public class DBLogger implements Logger{
+	@Override
+	public void log(String message) {
+		System.out.println("[DB] DB에 로그 저장 : " + message);
+	}
+
+}
