55b7e83fa282d8fb6bd925a269119f2510235c39
cusgi2003 <dalraexq1@gmail.com>
2026-04-08 10:55:28 +0900

    Create ConsoleLogger.java

---
+++ java/ex_0407/src/ex01_reflection/log/ConsoleLogger.java
@@ -0,0 +1,9 @@
+package ex01_reflection.log;
+
+public class ConsoleLogger implements Logger{
+
+	@Override
+	public void log(String message) {
+		/System.out.println("[Consle] 로그 출력 : message");
+	}
+}
