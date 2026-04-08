2c859551f9f1596f206fb23a588a8c98115bc4c1
cusgi2003 <dalraexq1@gmail.com>
2026-04-08 17:14:12 +0900

    Create WildCards.java

---
+++ java/ex_0408/src/ex91_generic/WildCards.java
@@ -0,0 +1,36 @@
+package ex91_generic;
+
+import java.util.List;
+
+// 와일드카드
+// 제네릭 타입에서 "정확한 타입은 모르겠지만, 어떤 범위 안에 있는 타입이다."
+// 라고 표현할 때 사용한다
+// List<String> : 정확히 String을 담을 수 있는 리스트
+// List<Integer> : 정확히 Integer를 담는 리스트
+
+// 와일드카드의 기본문법
+// <?> : 타입을 모름, 아무 타입이나 가능함
+// <? extends T> : T 또는 T의 자식 타입만 가능하다 ( 상한에 제한을 둔다 )
+// <? super T> : T 또는 T의 부모 타입만 가능
+
+// <T extends Number> : 타입한정 -> 타입을 정의하는 것
+
+// 와일드카드가 써있는것들은 대부분 읽기 전용이다
+// 와일드카드는 맥변수에서 사용이 되는 경우가 많다
+// 이미 만들어진 객체를 받을 때 사용한다
+public class WildCards {
+	
+	// ?에 어떤 타입이 들어올 지 알 수 없기 때문이다.
+	public void printList(List<?> list){
+		for(Object obj : list) {
+			System.out.println(obj);
+		}
+	}
+	
+	// 상한 제한 와일드 카드
+	public void printNumbers(List<? extends Number> list) {
+		for(Number obj : list) {
+			System.out.println(obj);
+		}
+	}
+}
