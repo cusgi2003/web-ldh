f856d9fa54fc94feb1ddf3fbd7ce237ef2a689de
cusgi2003 <dalraexq1@gmail.com>
2026-04-08 16:35:54 +0900

    Update Main.java

--- java/ex_0408/src/ex91_generic/Main.java
+++ java/ex_0408/src/ex91_generic/Main.java
@@ -1,5 +1,30 @@
 package ex91_generic;
 
 public class Main {
+	public static void main(String[] args) {
+		// 제네릭에 타입을 넣을 수 있는 것은 좋은데
+		// 아무 탕비이나 넣어도 된다는 것이 문제
+		Calculator<Number> cal1 = new Calculator<Number>();
+		Calculator<Integer> cal2 = new Calculator<Integer>();
+		Calculator<Double> cal3 = new Calculator<Double>();
+		Calculator<Long> cal4 = new Calculator<Long>();
+		
+		// 어떤 기능을 만들 때는
+		// 특정 성질을 가진 타입만 바당야 안전하다는 문제가 생긴다
+		
+		// 제네릭에 넣을 수 있는 타입의 범위를 한정
+		
+		// 1. 잘못된 타입을 미리 막을 수 있다
+		// 2. 상위 타입의 기능을 사용할 수 있다
 
+		DeviceManager<Tv> tvManager = new DeviceManager<Tv>(new Tv());
+		DeviceManager<Audio> audioDeviceManager = new DeviceManager<Audio>(new Audio());
+		
+		tvManager.powerOn();
+		audioDeviceManager.powerOn();
+		
+		Box<BoxType>box = new Box<>();
+		
+		
+	}
 }
