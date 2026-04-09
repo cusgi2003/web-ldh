0e558c8a294c031a53be626cb424a67710820c94
cusgi2003 <dalraexq1@gmail.com>
2026-04-09 17:53:39 +0900

    Create UserInfo.java

---
+++ java/ex_0409/src/ex01_list/arraylist/exam/UserInfo.java
@@ -0,0 +1,20 @@
+package ex01_list.arraylist.exam;
+
+public class UserInfo {
+	private String userId;
+	private int password;
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public int getPassword() {
+		return password;
+	}
+}
