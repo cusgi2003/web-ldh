73c23a859b0422bad797063514221bd4bd0c7da1
cusgi2003 <dalraexq1@gmail.com>
2026-04-09 15:50:44 +0900

    Create Member.java

---
+++ java/ex_0409/src/ex01_list/arraylist/Member.java
@@ -0,0 +1,20 @@
+package ex01_list.arraylist;
+
+public class Member {
+	private String name;
+	private int age;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+}
