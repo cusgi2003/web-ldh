fdad5d6d9f6247ace522f918ac21b922a2685bd3
cusgi2003 <dalraexq1@gmail.com>
2026-04-10 14:47:21 +0900

    Create DiscountCalculator.java

---
+++ java/exam/src/exam/ex1/DiscountCalculator.java
@@ -0,0 +1,20 @@
+package exam.ex1;
+
+public class DiscountCalculator {
+	public static void main(String[] args) {
+		int price = 10000;
+	    String grade = "VIP";
+	    int discountedPrice = 0;
+	 
+	    // [A] 아래에 설계서에 맞는 조건문을 작성하시오.
+
+	    if (grade.equals("VIP")) { 
+	           discountedPrice = (int)(price * 0.8); 
+	       } 
+	       System.out.println("회원의 등급 : " + grade);
+	       System.out.println("최종 할인된 금액 : " + discountedPrice);
+
+	   }
+	
+		
+	}
