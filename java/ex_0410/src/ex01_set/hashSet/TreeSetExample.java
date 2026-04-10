808fd0ffdf922eeb8b04b4d9241def93612c61df
cusgi2003 <dalraexq1@gmail.com>
2026-04-10 16:42:06 +0900

    Update TreeSetExample.java

--- java/ex_0410/src/ex01_set/hashSet/TreeSetExample.java
+++ java/ex_0410/src/ex01_set/hashSet/TreeSetExample.java
@@ -17,6 +17,21 @@ public class TreeSetExample {
 		System.out.println(scores);
 		
 		// TreeSet은 정렬되어 있기 때문에 특정 범위의 데이터 검색이 편하다
-		System.out.println(scores.first());
+		System.out.println(scores.first());	// 가장 작은 값
+		System.out.println(scores.last());
+		
+		// lower(x)
+		// x보다 가까운 값중 가장 가까운 값
+		System.out.println(scores.lower(30));
+		
+		// higher(x)
+		// x보다 큰 값 중 가장 까까운 값
+		System.out.println(scores.higher(30));
+		
+		// 중위 순회
+		// 왼쪽 -> 부모 -> 오른쪽
+		// 왼쪽끝 -> 10
+		// 루트 -> 30
+		// 오른쪽 50
 	}
 }
