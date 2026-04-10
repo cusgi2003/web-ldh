ca66bc22c1d021ed6fd6aae2d89043a2a151599a
cusgi2003 <dalraexq1@gmail.com>
2026-04-10 18:25:42 +0900

    Create Bingo.java

---
+++ java/ex_0410/src/ex03_collections/Bingo.java
@@ -0,0 +1,28 @@
+package ex03_collections;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Bingo {
+	public static void main(String[] args) {
+		// 1 ~ 50사이의 난수가 있다
+		// 5 x 5 의 빙고판에 25개의 숫자를 채운다
+		// 겹치면 안됨
+		
+		// 만약 숫자가 오름차순으로 들어간다면 섞어주기
+		
+		// ex)
+		
+		// 5	7	23	9	13
+		// 41	38	17	10	3
+		// 26	...
+		
+		Set<Integer> bingo = new HashSet<Integer>();
+		while(bingo.size() != 25) {
+			bingo.add((int)((Math.random()*50)+1));
+		}
+		
+		
+	}
+}
