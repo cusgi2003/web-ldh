fa5e8977a8e47b4514e8ce4bf55a9d297c25f93a
cusgi2003 <dalraexq1@gmail.com>
2026-04-13 10:45:46 +0900

    Update Bingo.java

--- java/ex_0410/src/ex03_collections/Bingo.java
+++ java/ex_0410/src/ex03_collections/Bingo.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Set;
 
 public class Bingo {
+	static int[][] board = new int[5][5];
+	
 	public static void main(String[] args) {
 		// 1 ~ 50사이의 난수가 있다
 		// 5 x 5 의 빙고판에 25개의 숫자를 채운다
@@ -27,12 +29,52 @@ public class Bingo {
 		List<Integer> list = new ArrayList<>(bingo	);
 		Collections.shuffle(list);
 		
-		for(int i = 0; i < 25; i++) {
-			System.out.print(list.get(i) + " ");
-			if((i+1) % 5 == 0) {
-				System.out.println();
+	    int index = 0; // 리스트의 인덱스를 추적
+	    
+	    for(int i = 0; i < 5; i++) {
+	        for(int j = 0; j < 5; j++) {
+	            board[i][j] = list.get(index++);
+	        }
+	    }
+	}
+	// 숫자르 체크하는 메서드
+	// 매개변수 : int number;  ㅡ   
+	// 메서드명 : markNumber'
+	// 숫자를 넘겨받으면 해당 빙고칸의 숫자를 0으로 반바꾸고 true를 반환
+	// 아니면 false를 반환
+	public static boolean markNumber(int number) {
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				if(board[i][j] == number) {
+					board[i][j] = 0;
+					return true;
+				}
 			}
 		}
-		
+		return false;
 	}
-}
+	
+	// 현재 빙고판의 상태를 출력하는 printBoard() 메서드 만들기
+	// 체크된 카드의 숫자는 X로 출력
+	
+	// 08 14 25 37 42
+	// 11 03 50 29 16
+	//  x 22 18 09 31
+	// 44 10 27 35 01
+	// 13 06 40 19 47
+	
+	public void printBoard() {
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board.length; j++) {
+				if(board[i][j] == 0) {
+					System.out.printf("%3s", "X");
+				} else {
+					System.out.printf("%3d", board[i][j]);
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("-----------------");
+	}
+	
+}			
