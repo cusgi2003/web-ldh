3b4ef1b05989a4ec394880b919dd836fe8b82c58
cusgi2003 <dalraexq1@gmail.com>
2026-04-14 11:19:48 +0900

    Update CalculatorService.java

--- java/ex_0414/src/ex01_lambda/CalculatorService.java
+++ java/ex_0414/src/ex01_lambda/CalculatorService.java
@@ -2,4 +2,22 @@ package ex01_lambda;
 
 public class CalculatorService {
 
+	// 람다를 매개변수로 받는 메서드
+	// 식 자체를 매개변수로 넘길 수 있다
+	public void execute(Calculator caclCalculator) {
+		int result = caclCalculator.calc(10, 20);
+		System.out.println("결과 : " + result);
+	}
+
+	// 람다를 반환하는 메서드
+	// 매개변수로 "add" 문자열이 넘어오면 두 수를 더해서 반환
+	// "sub" 문자열이 넘어오면 두 수를 빼서 반환
+	public Calculator getCalculator(String type) {
+		if (type.equals("add")) {
+			return (a, b) -> (a + b);
+		} else if (type.equals("sub")) {
+			return (a, b) -> (a - b);
+		}
+	}
+
 }
