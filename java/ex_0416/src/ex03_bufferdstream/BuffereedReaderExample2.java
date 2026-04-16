1f255bb5eae3d095f56a8221e2bdbace0e8598b0
cusgi2003 <dalraexq1@gmail.com>
2026-04-16 18:06:39 +0900

    Update BuffereedReaderExample2.java

--- java/ex_0416/src/ex03_bufferdstream/BuffereedReaderExample2.java
+++ java/ex_0416/src/ex03_bufferdstream/BuffereedReaderExample2.java
@@ -1,5 +1,8 @@
 package ex03_bufferdstream;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BuffereedReaderExample2 {
 	public static void main(String[] args) {
 		// Scanner 
@@ -11,5 +14,41 @@ public class BuffereedReaderExample2 {
 		
 		//BufferedReader
 		// 버퍼를 사용하기 때문에 속도가 빠르다
+		// 오직 문자열 단위로만 읽는다
+		// 다른 타입으로 사용하려면 번거롭지만 직접 바꿔줘야 한다
+		// 보조스트림이기 때문에 기반 스트림이 필요하다
+		
+		// System.in : 표준 입력 스트림(바이트 기반 스트림)
+		// 문자나 문자열을 편하게 읽는 용도가 아니라 바이트단위로 데이터를 읽는 통로
+		// 그래서 InputStreanReader를 사용해서 문자 스트림으로 변환해준다.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String name = null;
+		int age = 0;
+		
+		String info = null;
+		 
+		try {
+//			System.out.print("이름을 입력하세요");
+//			name = br.readLine();
+//			System.out.print("나이를 입력하세요");
+//			age = Integer.parseInt(br.readLine());
+			
+			System.out.print("이름과 나이를 입력하세요 : ");
+			info = br.readLine();
+			
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+//		System.out.println("입력한 이름 : " + name);
+//		System.out.println("입력한 나이 : " + age);
+		
+//		System.out.println("입력한 값 : " + info);
+		String[] arr = info.split(" ");
+		System.out.println("이름 :" + arr[0]);
+		System.out.println("나이 : " + arr[1]);
 	}
 }
