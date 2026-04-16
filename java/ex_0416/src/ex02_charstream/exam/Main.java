02f9b1c27e2f2345cc3f4cb7f42375230ccb8596
cusgi2003 <dalraexq1@gmail.com>
2026-04-16 16:12:44 +0900

    Update Main.java

--- java/ex_0416/src/ex02_charstream/exam/Main.java
+++ java/ex_0416/src/ex02_charstream/exam/Main.java
@@ -6,48 +6,71 @@ import java.io.FileOutputStream;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		
+
 		File f = new File("users.txt");
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("1. 회원가입 | 2. 로그인 | 3. 종료");
-			
+
 			System.out.print("선택 : ");
-			int choice = sc.nextInt();
-			
-			if(choice == 1) {
+			int choice = Integer.parseInt(sc.next());
+
+			if (choice == 1) {
 				System.out.print("아이디 : ");
-				String id = sc.nextLine();
+				String id = sc.next();
 				System.out.print("비밀번호 : ");
-				String password = sc.nextLine();
-				
-				User newUser = new User(id,password);
+				String password = sc.next();
+
+				User newUser = new User(id, password);
 				FileOutputStream fos = new FileOutputStream(f);
 				fos.write(newUser.toByteData());
 				fos.close();
-				
+
 				System.out.println("회원가입 완료");
-			} else if(choice == 2) {
+			} else if (choice == 2) {
 				System.out.print("아이디 : ");
 				String id = sc.nextLine();
+				sc.nextLine();
 				System.out.print("비밀번호 : ");
 				String password = sc.nextLine();
-				
-				if(!f.exists()) {
+
+				if (!f.exists()) {
 					System.out.println("가입된 회원이 없습니다");
 					continue;
 				}
-				
+
 				// 파일 전체 읽기
 				FileInputStream fis = new FileInputStream(f);
 				byte[] buffer = new byte[(int) f.length()];
 				fis.read(buffer);
 				fis.close();
-				
+
 				String allUsersData = new String(buffer);
 				String[] lines = allUsersData.split("\n");
+
+				boolean loginSuccess = false;
+
+				for (String line : lines) {
+					if (line.isEmpty())
+						continue;
+					String[] parts = line.split(":");
+
+					if (parts[0].equals(id) && parts[1].equals(password)) {
+						loginSuccess = true;
+						break;
+					}
+				}
+
+				if (loginSuccess) {
+					System.out.println("로그인 성공! 환영합니다");
+				} else {
+					System.out.println("로그인 실패 : 아이디 또는 비밀번호를 확인하세요.");
+				}
+			} else if (choice == 3) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
 			}
 		}
 
