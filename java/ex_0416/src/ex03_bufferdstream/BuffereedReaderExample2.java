4dba3036d1d766468815d87b0c3d93ddbe9acad2
cusgi2003 <dalraexq1@gmail.com>
2026-04-16 16:15:47 +0900

    Update BuffereedReaderExample2.java

--- java/ex_0416/src/ex03_bufferdstream/BuffereedReaderExample2.java
+++ java/ex_0416/src/ex03_bufferdstream/BuffereedReaderExample2.java
@@ -1,5 +1,15 @@
 package ex03_bufferdstream;
 
 public class BuffereedReaderExample2 {
-
+	public static void main(String[] args) {
+		// Scanner 
+		// 속도가 느리다
+		// nextInt, nextDouble() 등 입력과 동시에 데이터 타입을
+		// 변환해주는 편의 기능이 많다
+		// 하지만 내부적으로 정규표현식을 사용하여 입력을 분석하기 때문에
+		// 속도가 느려진느 원인이 된다
+		
+		//BufferedReader
+		// 버퍼를 사용하기 때문에 속도가 빠르다
+	}
 }
