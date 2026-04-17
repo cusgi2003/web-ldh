22df8d71a2f1e57b1acf8015578944c40986e3f4
cusgi2003 <dalraexq1@gmail.com>
2026-04-17 12:09:12 +0900

    Create Member.java

---
+++ java/exam/src/exam2/Member.java
@@ -0,0 +1,33 @@
+package exam2;
+
+public class Member {
+	private String id;
+	private String name;
+	private int age;
+
+	public Member(String id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void printInfo() {
+		System.out.println("아이디 : " + this.id + ", 이름 : " + this.name + ", 나이 : " + this.age);
+	}
+}
