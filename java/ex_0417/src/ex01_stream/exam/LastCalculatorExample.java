c6ac62aa2253f6c2dba9dc2f149ca74595d8efc2
cusgi2003 <dalraexq1@gmail.com>
2026-04-17 17:43:51 +0900

    Update LastCalculatorExample.java

--- java/ex_0417/src/ex01_stream/exam/LastCalculatorExample.java
+++ java/ex_0417/src/ex01_stream/exam/LastCalculatorExample.java
@@ -2,6 +2,10 @@ package ex01_stream.exam;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LastCalculatorExample {
 	public static void main(String[] args) {
@@ -40,13 +44,77 @@ public class LastCalculatorExample {
 		result = numbers.stream().allMatch(x -> x % 2 == 0);
 
 		System.out.println("결과 : " + result);
-		
+
 		// noneMatch()
 		// 모든 요소가 조건에 하나도 맞지 않아야 함
-		
+
 		result = numbers.stream().noneMatch(x -> x % 2 == 0);
 
 		System.out.println("결과 : " + result);
+
+		// 결과를 모으는 최종 연산자
+		// collect()
+		// 스트림의 결과를 리스트, 셋, 맵 등으로 모으는 최종 연산
+
+		words.stream().filter(s -> s.length() >= 5).collect(Collectors.toList());
+
+		// Collectors.toList() : 결과를 리스트로 반환
+		// Collectors.toSet() : 셋으로 반환
+		// Collectors.toMap(k,v)
+
+		// 누적해서 합치기
+		// reduce()
+
+		int sum = numbers.stream().reduce(0, (a, b) -> a + b);
+		// 시작값 : 0
+		// 1. 시작값이 a로 들어간다
+		// 2. 컬렉션의 요소들은 b로 들어간다
+		// 3. 더한 값을 a에 넣는다
+		// 4. 다음 요소를 b에 넣는다
+		// 5. 모든 요소를 순회할 때까지 반복
+		
+		System.out.println(sum);
+		
+		// 누적합
+		// 곱셈 누적
+		// 최대값 / 최소값
+		// 문자열 연결
+		
+		// toArray()
+		// 스트림 결과를 배열로 반환
+		Object[] arr = numbers.stream().toArray();
+		
+		// 기본형 스트림 전용 최종 연산
+		
+		// sum()
+		sum = IntStream.rangeClosed(1, 5).sum();
+		System.out.println("결과 : " + sum);
+		
+		// average()
+		OptionalDouble avg = IntStream.of(10,20,30).average();
+		
+		// Optional 클래스
+		// 자바에서 값이 있을 수도 있고 없을 수도 있다는 상황을
+		// 안전하게 표현하기 위한 클래스
+		
+		// 객체에 값이 들어있지 않으면 NPE가 발생한다
+		
+		// 값이 안들어있으면 기본값을 반환
+		double average = avg.orElse(0.0);
+		
+		// ifPresent()
+		// 값이 있을때만 실행
+		avg.ifPresent(System.out::println);
+		
+		// max()
+		// 최대값 구하기
+		OptionalInt max2 = IntStream.of(7,3,2,9).max();
+//		System.out.println(max2.getAsInt());
+		max2.ifPresent(System.out::println);
+		
+		// min()
+		OptionalInt min = IntStream.of(7,3,2,9).max();
+		min.ifPresent(System.out::println);
 		
 	}
 }
